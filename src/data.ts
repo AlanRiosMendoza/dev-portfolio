110f0919d4c50b86aa001af6794eee024ba4afe8
Alan Rios Mendoza <alan.rios.hd@gmail.com>
2025-05-31 16:10:56 -0500

    feat: refactor layout and navigation components, add theme toggle functionality, and update project sections

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export const data = {
+    name: "Alan Rios Mendoza",
+    email: "alan.rios.hd@gmail.com",
+    role: "Desarrollador Backend",
+    status: "Disponible para trabajar",
+    avatar: "https://res-console.cloudinary.com/dkhh1qdbr/media_explorer_thumbnails/7d07994cb2a15cc873d35c902acef5dc/detailed",
+    cv: "https://drive.google.com/file/d/1_NzYD_MeRGAY8_Pwt0XdEoFs3bLatHYr/view?usp=sharing",
+    location: {
+        city: "Quito",
+        country: "Ecuador"
+    },
+    links: {
+        github: "https://github.com/AlanRiosMendoza/",
+        linkedin: "https://www.linkedin.com/in/alan-rm/",
+        x: "https://x.com/alan_ri0s",
+    },
+    about: `Soy un desarrollador con experiencia en Node.js, MongoDB y Express. Me enfoco en crear APIs REST y soluciones escalables.`,
+    skills: ["JavaScript", "Node.js", "MongoDB", "Astro", "TailwindCSS", "Express", "Docker", "JWT", "MySQL"],
+    experience: [
+        {
+            title: "Backend Developer",
+            company: "TechCorp",
+            period: "2023 - Actualidad",
+            description: "Desarrollo de APIs en Node.js con MongoDB y JWT."
+        },
+        {
+            title: "Full Stack Dev",
+            company: "Freelance",
+            period: "2021 - 2023",
+            description: "Proyectos de sitios web, autenticación, y despliegue con Docker."
+        }
+    ],
+    projects: [
+        {
+            name: "BitLink",
+            url: "https://btlk.vercel.app/",
+            img: "https://res-console.cloudinary.com/dkhh1qdbr/media_explorer_thumbnails/5c08064951a46ec38be97bbff3514b29/detailed",
+            github: "https://github.com/AlanRiosMendoza/bitlink",
+            description: "Acortador de enlaces con estadísticas y autenticación.",
+            tech: ["Astro", "TailwindCSS", "MySQL", "Clerk", "Turso DB"]
+        },
+    ]
+};
